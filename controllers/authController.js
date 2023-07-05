2ce5fc366717f8635e9ea5433ebed5437c950f00
Hardik Silawat <hardiksilawat0902@.gmail.com>
2023-07-05 14:11:46 +0530

    re.send added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
-exports.registerController = async () => {};
+exports.registerController = async (req, res) => {
+  res.send("MERN STACK CHATBOT");
+};
 exports.loginController = async () => {};
 exports.logOutController = async () => {};
